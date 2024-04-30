25364f8324ea85ca2c4656fc0c9edafbf3917b0d
Olfa <olfa.medimegh@insat.ucar.tn>
2024-04-30 21:12:46 +0100

    Initial commit: project+schema

---
+++ backend/src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [AuthModule, UserModule,
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule, AppModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      useFactory: async (
+        configService: ConfigService,
+      ) => {
+        return {
+          installSubscriptionHandlers: true,
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+          };          
+        },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/auth/auth.resolver.ts
@@ -0,0 +1,9 @@
+import { Resolver, Query} from '@nestjs/graphql';
+
+@Resolver()
+export class AuthResolver {
+@Query(() => String)
+  async hello() {
+    return 'hello';
+  }
+}
