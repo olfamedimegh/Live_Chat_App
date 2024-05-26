1eb7abd62132e950f56b060489af7fe521b6b275
IdrisOmit <saddi.idris@gmail.com>
2024-05-26 15:33:58 +0100

    ui: authentication componenet

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import MainLayout from "../layouts/Mainlayout";
 import Sidebar from "../components/Sidebar";
 import ProtectedRoutes from "../components/ProtectedRoutes";
+import AuthOverlay from "../components/AuthOverlay";
 
 function Home() {
     return (
         <MainLayout>
             <>
+                <AuthOverlay/>
                 <Sidebar/>
                 HOME_PAGE
             </>
