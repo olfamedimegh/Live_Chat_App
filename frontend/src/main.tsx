d0c76969294108610eb978807b251ba1597fbb09
asmaelhadj <asma.elhadj@insat.ucar.tn>
2024-05-26 12:54:04 +0100

    apolloClient+issue to be handled

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { ApolloProvider } from '@apollo/client'
+import { client } from "./apolloClient"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
